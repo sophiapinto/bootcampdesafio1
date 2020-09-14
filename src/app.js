58fb84a2649e501e8ab0383bc11d8f1c968148a1
Sophia Pinto <sarahsophiapinto@hotmail.com>
2020-09-14 20:42:36 -0300

    tentativa de ajuste do delete

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-const { uuid } = require("uuidv4");
+const { isuuid } = require("uuidv4");
 
 // const { v4: uuid } = require('uuid');
 
@@ -13,6 +12,15 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
+  //Rota que lista todos os repositórios;
+  //retornado um array com todos os repositórios que foram criados até o momento.
+
+  const { title } = request.query;
+  
+  const repositories = title
+  ? repositories.filter(repository => repository.title.includes(title))
+  : repositories;
+
   return response.json(repositories);
 });
 
@@ -24,7 +32,7 @@ app.post("/repositories", (request, response) => {
     title,
     url,
     techs,
-    likes: 0,
+    likes: 0, //like deve sempre iniciar em 0
   };
 
   repositories.push(repository);
@@ -33,20 +41,45 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  //// A rota deve alterar apenas o title,
+  //a url e as techs do repositório que possua o id igual
+  //ao id presente nos parâmetros da rota.
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+  //A rota deve deletar o repositório com o id presente nos parâmetros da rota;
+  //1º parâmetro: index de onde deve iniciar a remoção
+  //2º parâmetro: a quantidade de valores removidos.
+  
+  const { id } = request.params;
+
+  //const repository = repositories.body.find((r) => r.id === response.body.id);
+  const repositoryIndex = repositories.findIndex (repository => repository.id ==id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error:'Project not found'})
+  }
+
+  repositories.splice(repositoryIndex,1);
+
+  return response.status(204).send();
+
+  );
+
+  //Splice: Altera o array, removendo os valores dentro dele e substituindo por outros valores
 
 app.post("/repositories/:id/like", (request, response) => {
+    //A rota deve aumentar o número de likes do repositório específico escolhido
+    //através do id presente nos parâmetros da rota,
+    //a cada chamada dessa rota, o número de likes deve ser aumentado em 1;
+  
   const { id } = request.params;
 
   const repository = repositories.find(repository => repository.id ===id);
   
   if (!repository) {
-    return response.status(400).send();
+    return response.status(400).send(); //validação de id
   }
   repository.likes += 1;
 
